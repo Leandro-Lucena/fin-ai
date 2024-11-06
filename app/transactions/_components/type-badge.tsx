e2f2c6d4e7d37333e25ef260875debfaa923a165
Leandro-Lucena <leandroribeirolucena@gmail.com>
2024-11-06 17:00:16 -0300

    feat: ensure transaction table labels are rendered properly

--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -17,7 +17,7 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   }
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="text-danger bg-danger bg-opacity-10 font-bold">
+      <Badge className="text-danger bg-danger hover:bg-danger bg-opacity-10 font-bold hover:bg-opacity-10">
         <CircleIcon className="mr-2 fill-destructive" size={10} />
         Despesa
       </Badge>
