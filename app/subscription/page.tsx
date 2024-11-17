17ee2a7d2a54bb5a5d12f789f9fef187dafb5cee
Leandro-Lucena <leandroribeirolucena@gmail.com>
2024-11-16 21:38:24 -0300

    chore: adjust price of the premium plan

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -70,7 +70,7 @@ const SubscriptionPage = async () => {
               </h2>
               <div className="flex items-center justify-center gap-3">
                 <span className="text-4xl">R$</span>
-                <span className="text-6xl font-semibold">19</span>
+                <span className="text-6xl font-semibold">12</span>
                 <span className="text-2xl text-muted-foreground">/mês</span>
               </div>
             </CardHeader>
